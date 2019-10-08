7ec9ca6d813a57cbb807a53e9ec5d20e2aa2542c
aarias89 <andresar11@gmail.com>
2019-10-08 16:12:16 -0400

    Updates

---
+++ component---src-pages-portfolio-js-4ad929c1010f17efde75.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{256:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(57),r=a(253),c=a(252);t.default=function(){return o.a.createElement(r.a,null,o.a.createElement(c.a,{title:"Portfolio"}),o.a.createElement("h1",null,"Hi from Portfolio Page"),o.a.createElement("p",null,"Welcome to the page... under construction "),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-portfolio-js-4ad929c1010f17efde75.js.map
---
+++ styles-c02f8f95aaee89de7da6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{287:function(n,w,o){}}]);
+//# sourceMappingURL=styles-c02f8f95aaee89de7da6.js.map
