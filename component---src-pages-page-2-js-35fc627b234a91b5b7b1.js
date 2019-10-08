cabc56cb5ba57baadb370e30711810ddea277bd6
aarias89 <andresar11@gmail.com>
2019-10-07 20:55:07 -0400

    Updates

---
+++ component---src-pages-page-2-js-35fc627b234a91b5b7b1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{305:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(63),c=a(303),r=a(302);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-35fc627b234a91b5b7b1.js.map
---
+++ component---src-pages-portfolio-js-295b44de229aea9b3414.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{306:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(63),r=a(303),c=a(302);t.default=function(){return o.a.createElement(r.a,null,o.a.createElement(c.a,{title:"Portfolio"}),o.a.createElement("h1",null,"Hi from Portfolio Page"),o.a.createElement("p",null,"Welcome to the page... under construction "),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-portfolio-js-295b44de229aea9b3414.js.map
