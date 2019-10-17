8ca84c4be1a48242c2c901a52a2e68ee0fd5e414
aarias89 <andresar11@gmail.com>
2019-10-17 16:10:51 -0400

    Updates

---
+++ component---src-pages-page-2-js-6ccb9a5b724d58e3a8e5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{259:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(59),c=a(255),r=a(253);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-6ccb9a5b724d58e3a8e5.js.map
---
+++ styles-8bd2c78d2d7521c824e2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{292:function(n,w,o){}}]);
+//# sourceMappingURL=styles-8bd2c78d2d7521c824e2.js.map
