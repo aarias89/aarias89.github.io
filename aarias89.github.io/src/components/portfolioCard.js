798a247214052a6d250c782fba8558bbdb953c5f
aarias89 <andresar11@gmail.com>
2019-10-17 15:43:04 -0400

    Add button render conditional code based on portfolio cta view boolean

--- aarias89.github.io/src/components/portfolioCard.js
+++ aarias89.github.io/src/components/portfolioCard.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
-import { Container,Card, Button } from "react-bootstrap"
+import { Container, Card, Button } from "react-bootstrap"
 import { Link } from "gatsby"
-
 
 export default class PortfolioCard extends Component {
   constructor(props) {
@@ -9,24 +8,42 @@ export default class PortfolioCard extends Component {
   }
 
   render() {
-    const { cardImg, cardTitle, cardDescription, button } = this.props.project
-    let imgRoute = require('../images/portfolioImages/' + cardImg);
-    
+    const { cardImg, cardTitle, cardDescription } = this.props.project
+
+    let imgRoute = require("../images/portfolioImages/" + cardImg)
+
+    const ctas = Object.entries(this.props.project.button).filter(cta => {
+      return cta[1].view === true
+    })
+
+    const renderButton = ctas.map(cta => {
+      return (
+        <a key={cta[1].copy} target="_blank" href={cta[1].url}>
+          <Button className="card-button" size="sm" variant="primary">
+            {cta[1].copy}
+          </Button>
+        </a>
+      )
+    })
+
     return (
       // <Container xs={12} md={6} lg={4}>
-        
-        <Card className="portfolio-card">
-          <br />
-          <Card.Img variant="top" src={imgRoute} />
-          <Card.Body>
-            <Card.Title>{cardTitle}</Card.Title>
-            <Card.Text>{cardDescription}</Card.Text>
-            <a target="_blank" href={button.cta1.url}><Button className="card-button" size="sm" variant="primary">{button.cta1.copy}</Button></a>
-            <a target="_blank" href={button.cta2.url}><Button className="card-button" size="sm" variant="primary">{button.cta2.copy}</Button></a>
-            <a target="_blank" href={button.cta3.url}><Button className="card-button" size="sm" variant="primary">{button.cta3.copy}</Button></a>
-          </Card.Body>
-          <br />
-        </Card>
+
+      <Card className="portfolio-card">
+        <br />
+        <Card.Img variant="top" src={imgRoute} />
+        <Card.Body>
+          <Card.Title>{cardTitle}</Card.Title>
+          <Card.Text>{cardDescription}</Card.Text>
+          {/* <a target="_blank" href={button.cta1.url}>
+            <Button className="card-button" size="sm" variant="primary">
+              {button.cta1.copy}
+            </Button>
+          </a> */}
+          {renderButton}
+        </Card.Body>
+        <br />
+      </Card>
     )
   }
 }
