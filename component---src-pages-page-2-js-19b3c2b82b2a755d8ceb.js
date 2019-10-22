7a6d81278ec394613e236acd4be69253a13be5f8
aarias89 <andresar11@gmail.com>
2019-10-22 13:38:54 -0400

    Updates

---
+++ component---src-pages-page-2-js-19b3c2b82b2a755d8ceb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{285:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(68),c=a(281),r=a(280);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-19b3c2b82b2a755d8ceb.js.map
